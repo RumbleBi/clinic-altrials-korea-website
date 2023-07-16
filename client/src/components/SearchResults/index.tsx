f157f450f0a23eea53258955341858f541c7c66b
RumbleBi <olzlel2000@gmail.com>
2023-07-16 16:03:33 +0900

    init: 프로젝트 초기구성완료

---
+++ client/src/components/SearchResults/index.tsx
@@ -0,0 +1,9 @@
+const SearchResults = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SearchResults;
---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,9 @@
+import { Wrap } from "./styled";
+
+export default function MainPage() {
+  return (
+    <Wrap>
+      <div>메인페이지</div>
+    </Wrap>
+  );
+}
